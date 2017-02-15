773c6ce6cac4c39697c15ea009c2f934f95a165e
Romesh <rselvanathan@omnifone.com>
2017-02-15 00:17:44 +0000

    Small Bug fix in ambigious type for AWS SQS clien

--- src/main/scala/com/romeshselvan/producers/AmazonSQSProducer.scala
+++ src/main/scala/com/romeshselvan/producers/AmazonSQSProducer.scala
@@ -10,5 +10,5 @@ import com.romeshselvan.defaults.SystemValues
   */
 object AmazonSQSProducer {
 
-  val amazonSQSClient = new AmazonSQSAsyncClient(new BasicAWSCredentials(SystemValues.AWS_ACCESS_KEY, SystemValues.AWS_SECRET_KEY)).withRegion(Region.getRegion(Regions.EU_WEST_1))
+  val amazonSQSClient: AmazonSQSAsyncClient = new AmazonSQSAsyncClient(new BasicAWSCredentials(SystemValues.AWS_ACCESS_KEY, SystemValues.AWS_SECRET_KEY)).withRegion(Region.getRegion(Regions.EU_WEST_1))
 }
